7a6208dd0898b1a10b577386497470607863d040
Paul Ryan <paul@simplycomplex.co>
2019-12-16 11:13:07 -0800

    initial ng-packagr model

---
+++ projects/cacheable/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of cacheable
+ */
+
+export * from './lib/cache-buster.decorator';
+export * from './lib/cacheable.decorator';
+export * from './lib/promise.cache-buster.decorator';
+export * from './lib/promise.cacheable.decorator';
